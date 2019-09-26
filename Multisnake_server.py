6722692db6f21f268feee485be3fc74411b38f24
TilerDurdan <53877009+TilerDurdan@users.noreply.github.com>
2019-09-26 17:42:17 +0300

    Add files via upload

--- Multisnake_server.py
+++ Multisnake_server.py
@@ -1,4 +1,85 @@
 import socket
+import threading
+import socket
+from _thread import *
+import pickle
+import threading
+from multiprocessing import Process, Queue, Lock, Event, current_process
+import queue
+import pygame
+import uuid
+import time
+
+
+class gamefield(object):
+
+    def __init__(self):
+        self.playerslist = {}
+
+
+class Player(object):
+
+    id = ''
+
+    def __init__(self):
+        self.id = self.newplayer()
+
+    def newplayer(self, playlist):
+
+        id = str(uuid.uuid4())[:5]
+        set_id = False
+
+        while not set_id:
+            if id not in playlist.playerslist:
+                playlist.playerslist[id] = self
+                set_id = True
+            else:
+                id = str(uuid.uuid4())[:5]
+
+        start_pos = self.gen_start(id)
+        return id
+
+
+def threaded_client(conn, chat_s, lock, mainq, event):
+    conn.send(pickle.dumps(chat_s))
+    global chat_server
+    reply = ""
+    while True:
+        try:
+            data = pickle.loads(conn.recv(1024))
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                event.wait()
+                lock.acquire()
+                try:
+                    '''chat_server.add_msg(data.list[-1])
+                    print(f'Trying to add {data.list[-1]} by {current_process().name}')'''
+                finally:
+                    lock.release()
+
+                '''myq.put(data.list[-1])
+
+                mainq.put('Update')'''
+                event.wait()
+
+                #time.sleep(0.1)
+                reply = chat_server
+
+                print("Received: ", data.list)
+                print("Sending : ", reply.list)
+
+            print("CHAT \n", chat_server.list)
+
+            conn.sendall(pickle.dumps(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+    return chat_s
 
 
 def get_ip():
@@ -15,3 +96,80 @@ def get_ip():
         s.close()
     return [IP, Port]
 
+
+# записали локальный адрес в список
+whereami = get_ip()
+# пишем где мы
+print(f"Server started at {whereami[0]}:{whereami[1]}")
+
+server = str(whereami[0])
+port = whereami[1]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# стартанули сервер
+try:
+    s.bind((server, port))
+except socket.error as e:
+    print(str(e))
+
+# слушаем, ждём
+s.listen()
+print("Waiting for a connection, Server Started")
+
+# служебные штуки для очередей и событий
+lock = Lock()
+maingame = gamefield()
+
+event = threading.Event()
+
+myq = queue.Queue()
+mainq = queue.Queue()
+
+# начало работы
+
+
+if __name__ == '__main__':
+
+    currentuser = 0
+
+
+
+    while True:
+
+        connection, addr = s.accept()
+        print("Connected to:", addr)
+
+        newconnection = threading.Thread(target=threaded_client, args=(connection, snake_server, lock, mainq, event))
+
+        newconnection.start()
+        event.set()
+        if not mainq.empty():
+            mainq.get()
+
+            """
+                def newplayer(self, data):
+
+        id =  str(uuid.uuid4())[:5]
+        set_id = False
+        Nick = data[6:]
+
+
+
+        while not set_id:
+            if id not in self.idlist:
+                self.idlist.append([id, Nick, start_pos])
+                set_id = True
+            else:
+                id = str(uuid.uuid4())[:5]
+
+        start_pos = self.gen_start(id)
+        return id
+            """
+
+            # вот тут из очереди надо будет гонять обновление карты относительно действий игроков из очереди
+            while not myq.empty():
+                taken = myq.get()
+                chat_server.add_msg(taken)
+                print(taken, ' from myq')
+        newconnection.join(1)
